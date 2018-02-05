d6f1163b81eb5641564bec6517b7478332ad04d9
kumarmudur <kumarmudur10@gmail.com>
2018-02-05 19:52:35 +0530

    unwanted files deleted

--- src/search-box.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  'search-control': {
-    'width': [{ 'unit': '%H', 'value': 0.3 }],
-    'margin': [{ 'unit': 'string', 'value': 'auto' }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'string', 'value': 'auto' }]
-  }
-});
